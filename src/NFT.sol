c7e8328dc673b6345d519334652b1fc93f10f4ad
Kenneth Smith <ksmit323@gmail.com>
2023-11-14 22:04:11 +0700

    Update NFT

--- src/NFT.sol
+++ src/NFT.sol
@@ -3,26 +3,59 @@ pragma solidity ^0.8.23;
 
 import "lib/solmate/src/tokens/ERC721.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
+error MintPriceNotPaid();
+error MaxSupply();
+error NonExistentTokenURI();
+error WeithdrawTransfer();
 
 /// @title Buzzkill NFT
-contract Buzzkill is ERC721 {
+contract Buzzkill is ERC721, Ownable {
 
-  uint256 public currentTokenID;
+  using Strings for uint256;
+  string public baseURI;
+  uint256 public currentTokenId;
+  uint256 public constant TOTAL_SUPPLY = 10_000;
+  uint256 public constant MINT_PRICE = 0.0073 ether;
 
   constructor(
     string memory _name, 
-    string memory _symbol
-  ) ERC721(_name, _symbol) {}
+    string memory _symbol,
+    string memory _baseURI
+  ) ERC721(_name, _symbol) {
+      baseURI = _baseURI;
+  }
 
   function mintTo(address recipient) public payable returns(uint256) {
-    uint256 newItemId = ++currentTokenID;
-    _safeMint(recipient, newItemId);
+    if (msg.value != MINT_PRICE) {
+      revert MintPriceNotPaid();
+    }
+
+    uint256 newTokenId = ++currentTokenId;
+
+    if (newTokenId > TOTAL_SUPPLY) {
+      revert MaxSupply();
+    }
+
+    _safeMint(recipient, newTokenId);
     return newItemId;
   }
 
-  function tokenURI(uint256 id) public view virtual override returns(string memory) {
-    return Strings.toString(id);
+  function tokenURI(uint256 tokenid) public view virtual override returns(string memory) {
+    if (ownerOf(tokenId) == address(0)) {
+      revert NonExistentTokenURI();
+    }
+
+    return bytes(baseURI.length) > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+  }
+
+  function withdrawPayments(address payable payyee) external onlyOwner {
+    uint256 balance = address(this).balance;
+    (bool transferTx, ) = payee.call{value: balance}("");
+    if (!transferTx) {
+      revert WeithdrawTransfer();
+    }
   }
 
 }
---
+++ test/NFT.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "forge-std/Test.sol";
+import "../src/NFT.sol";
+
+contract NFTTest is Test {}
